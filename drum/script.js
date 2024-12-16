cf0bc1cb66e0d546b9a4f68f860eb053b115466e
OpenCircleDeveloper <opencircledevelopers@gmail.com>
2024-12-16 09:06:50 +0500

    hello

--- drum/script.js
+++ drum/script.js
@@ -1,17 +1,9 @@
 var length = (document.querySelectorAll(".drum").length) - 1;
 var i = 0
 
-// for(var i =0 ; i<=length; i++){
-//     document.querySelectorAll("button")[i].addEventListener("click",function clickHandler(){
-//         alert("you clicked on button" + i)
-//     }) 
-// }
+for(var i =0 ; i<=length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function clickHandler(){
+        alert("you clicked on button" + i)
+    }) 
+}
     
-while(i<=length){
-    console.log(i)
-    document.querySelectorAll("button")[i].addEventListener("click",clickHandler);
-    i++
-}
-function clickHandler(){
-    alert("you clicked on button" + i)
-}
