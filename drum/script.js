d1d12514f9fef02928134413c8367fb918f0c0b8
OpenCircleDevelopers <opencircledevelopers@gmail.com>
2024-12-19 10:17:58 +0500

    Delete drum directory

--- drum/script.js
+++
@@ -1,9 +0,0 @@
-var length = (document.querySelectorAll(".drum").length) - 1;
-var i = 0
-
-for(var i =0 ; i<=length; i++){
-    document.querySelectorAll("button")[i].addEventListener("click",function clickHandler(){
-        alert("you click on button " + this.style.color = 'blue')
-    }) 
-}
-    
